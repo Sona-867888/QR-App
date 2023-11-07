45e82c76085009b0eca3bb8c6e3db1de228f29da
sonali <srivastavassonali@gmail.com>
2023-11-07 22:41:45 +0530

    2nd commit

--- Backened/src/controllers/saveQRCode.ts
+++ Backened/src/controllers/saveQRCode.ts
@@ -6,35 +6,7 @@ import * as QRCode from 'qrcode';
 
 
 
-export const saveqr = async (req: Request, res: Response) => {
-    
-    const { userId, text } = req.body as { userId: string; text: string };
-  
-    try {
-   
-      const user = await UserModel.findById(userId);
-    
-      if (!user) {
-       
-        return res.status(404).json({ message: 'User not found' });
-      }
-  
-      const qrCodeDataUrl = await QRCode.toDataURL(text);
-  
-     
-      const newQRCode = new QRCodeModel({
-        userId,
-        data: qrCodeDataUrl,
-        });
-  
-      const savedQRCode = await newQRCode.save();
-      res.status(201).json(savedQRCode);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Error generating and saving QR code' });
-    }
 
-  };
 
 
 
